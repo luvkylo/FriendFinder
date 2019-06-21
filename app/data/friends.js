23bf7f1ebacf8a55c2a5f5595c890620b6e909f5
Alvin Ng <alvinhewittalvin@gmail.com>
2019-06-20 19:44:16 -0700

    upload everything

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var person = [
+    {
+        name: "Ahmed",
+        photo: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = person;
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,41 @@
+var tableData = require("../data/friends");
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(tableData);
+    });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function (req, res) {
+        var totalDifference = 1000000;
+        var friend = {};
+        tableData.forEach(element => {
+            var num = element.scores.reduce((a, b) => Number(a) + Number(b));
+            if (Math.abs(num - req.body.scores.reduce((a, b) => Number(a) + Number(b), 0)) < totalDifference) {
+                totalDifference = Math.abs(num - req.body.scores.reduce((a, b) => Number(a) + Number(b), 0));
+                friend = element;
+            }
+        });
+
+        for (let i = 0; i < req.body.scores.length; i++) {
+            req.body.scores[i] = Number(req.body.scores[i]);
+        }
+
+        tableData.push(req.body);
+        res.json(friend);
+    });
+
+};
